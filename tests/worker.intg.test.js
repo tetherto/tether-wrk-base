b3255402cc40c9f2c0614ff9ab48f34c497f6cdd
agayen <abhijitgayen428@gmail.com>
2025-05-07 12:33:28 +0530

    storeDir as temp

--- tests/worker.intg.test.js
+++ tests/worker.intg.test.js
@@ -4,7 +4,6 @@ const Worker = require('../workers/base.wrk.tether.js')
 const path = require('path')
 const tmp = require('test-tmp')
 const { test, hook } = require('brittle')
-const fsp = require('fs').promises
 const RPC = require('@hyperswarm/rpc')
 
 let wrk = null
@@ -17,41 +16,18 @@ async function rpcReq (pubKey, met, data) {
   return JSON.parse(rep.toString())
 }
 
-async function processConfigFiles (dirPath) {
-  const entries = await fsp.readdir(dirPath, { withFileTypes: true })
-
-  for (const entry of entries) {
-    const fullPath = path.join(dirPath, entry.name)
-
-    if (entry.isDirectory()) {
-      await processConfigFiles(fullPath)
-    } else if (entry.isFile() && entry.name.endsWith('.example')) {
-      const newFilePath = fullPath.replace('.example', '')
-      await fsp.rename(fullPath, newFilePath)
-      console.log(`Renamed: ${fullPath} -> ${newFilePath}`)
-    }
-  }
-}
-
 hook('setup hook', async function (t) {
   const dir = await tmp(t)
   rpc = new RPC()
 
-  const sourceConfigPath = path.resolve(__dirname, '../config')
-  const destinationConfigPath = path.join(dir, 'config')
-
-  try {
-    await fsp.access(sourceConfigPath)
-    await fsp.cp(sourceConfigPath, destinationConfigPath, { recursive: true })
-    console.log(`Copied config folder to: ${destinationConfigPath}`)
-    await processConfigFiles(destinationConfigPath)
-  } catch (err) {
-    console.error('Config folder does not exist or cannot be accessed!')
-  }
-
   wrk = new Worker(
     {},
-    { env: 'test', root: path.resolve(dir, '.'), wtype: 'tether-wrk-base' }
+    {
+      env: 'test',
+      tmpdir: path.resolve(dir, '.'),
+      root: path.resolve(__dirname, '..'),
+      wtype: 'tether-wrk-base'
+    }
   )
   wrk.init()
 
@@ -64,8 +40,6 @@ test('worker test', async function (t) {
   const resp = await rpcReq(pubKey, 'ping', 'hello world')
   console.log(resp)
   t.is(resp, 'hello world')
-
-  console.log('rpc call - ping - done')
 })
 
 hook('teardown hook', async function (t) {
--- workers/base.wrk.tether.js
+++ workers/base.wrk.tether.js
@@ -9,9 +9,14 @@ class TetherWrkBase extends WrkBase {
     super.init()
 
     this.loadConf('common')
+    let storeDir = `store/${this.ctx.rack}`
+
+    if (this.ctx.env === 'test' && this.ctx.tmpdir) {
+      storeDir = `${this.ctx.tmpdir}/store/${this.ctx.rack}`
+    }
 
     this.setInitFacs([
-      ['fac', 'hp-svc-facs-store', 's0', 's0', { storeDir: `${this.ctx.root}/store/${this.ctx.rack}` }, 0],
+      ['fac', 'hp-svc-facs-store', 's0', 's0', { storeDir }, 0],
       ['fac', 'hp-svc-facs-net', 'r0', 'r0', () => ({ fac_store: this.store_s0 }), 1]
     ])
 
