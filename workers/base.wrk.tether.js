200091f86564adc8afb8ee1d122c5cf52287cec3
sarge <userkhen@gmail.com>
2025-08-31 21:51:42 +0500

    move to facility

--- workers/base.wrk.tether.js
+++ workers/base.wrk.tether.js
@@ -13,70 +13,12 @@ class TetherWrkBase extends WrkBase {
       ? `${this.ctx.tmpdir}/store/${this.storeDir || this.ctx.rack}`
       : `store/${this.storeDir || this.ctx.rack}`
 
+    const name = `wrk:proc:${this.ctx.wtype}:${process.pid}`
     this.setInitFacs([
       ['fac', 'hp-svc-facs-store', 's0', 's0', { storeDir }, 0],
       ['fac', 'hp-svc-facs-net', 'r0', 'r0', () => ({ fac_store: this.store_s0 }), 1]
     ])
 
-    const name = `wrk:proc:${this.ctx.wtype}:${process.pid}`
-
-    const stdout = pino.destination(1)
-    const stderr = pino.destination(2)
-    if (this.conf.lokiUrl && this.conf.loggingSecretKey && this.conf.loggingTopic) {
-      this.logger = pino(
-        {
-          name,
-          level: this.conf.debug || this.ctx.debug ? 'debug' : 'info',
-          enabled: this.ctx.logging ?? true,
-          transport: {
-            targets: [
-              {
-                target: './lib/pino-hyperswarm-exporter',
-                options: {
-                  topic: 'pino-logs-channel-1',
-                  app: name,
-                  secretKey: 'my-secret-key-123'
-                }
-              },
-              {
-                target: 'pino/file',
-                options: { destination: 1 },
-                level: 'info'
-              },
-              {
-                target: 'pino/file',
-                options: { destination: 2 },
-                level: 'error'
-              },
-              {
-                target: 'pino/file',
-                options: { destination: 2 },
-                level: 'warn'
-              },
-              {
-                target: 'pino/file',
-                options: { destination: 2 },
-                level: 'fatal'
-              }
-            ]
-          }
-        }
-      )
-    } else {
-      this.logger = pino(
-        {
-          name: `wrk:proc:${this.ctx.wtype}:${process.pid}`,
-          level: this.conf.debug || this.ctx.debug ? 'debug' : 'info',
-          enabled: this.ctx.logging ?? true
-        },
-        pino.multistream([
-          { level: 'info', stream: stdout },
-          { level: 'error', stream: stderr },
-          { level: 'warn', stream: stderr },
-          { level: 'fatal', stream: stderr }
-        ], { dedupe: true })
-      )
-    }
   }
 
   getRpcKey () {
